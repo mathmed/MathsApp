2c06fa89989db7fccc0828201fe81a19ea146361
mathmed <mateusmedeiros252525@hotmail.com>
2017-08-01 21:34:19 -0300

    Add files via upload

---
+++ index.android.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+const MathsApp = props => (
+  <App />
+)
+
+AppRegistry.registerComponent('MathsApp', () => MathsApp);
